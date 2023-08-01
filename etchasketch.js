8305b598b9d5597b1369b3f793d9b708311a67af
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-08-01 16:27:18 +0100

    What I have just done (tried to make the boxes change size) has not worked but it's hometime so TTYL

--- etchasketch.js
+++ etchasketch.js
@@ -1,15 +1,16 @@
 const container = document.querySelector("#container");
 const gridSizeButton = document.querySelector("#gridSizeButton");
 
-gridSizeButton.addEventListener("click", changeGridSize);
+//gridSizeButton.addEventListener("click", changeGridSize);
 
-makeGrid(16);
+makeGrid(30);
 
 function makeGrid(x) {
   for (let columns = 0; columns < x; columns++) {
     for (let rows = 0; rows < x; rows++) {
       const box = document.createElement("div");
       box.classList.add("sketch-divs");
+      box.setAttribute('style', 'aspect-ratio: 1/1; width: auto; height auto;');
       container.appendChild(box);
       box.addEventListener("mouseenter", function (e) {
         e.target.style.background = "pink";
