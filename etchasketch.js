080f8ab77caf8b504074372f4cc4d2993665d3fc
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-08-01 14:54:14 +0100

    Now it goes pink when you hover over it

--- etchasketch.js
+++ etchasketch.js
@@ -8,6 +8,9 @@ function makeGrid(x) {
       const box = document.createElement("div");
       box.classList.add("sketch-divs");
       container.appendChild(box);
+      box.addEventListener("mouseenter", function (e) {
+        e.target.style.background = "pink";
+      });
     }
   }
 }
