ab353435d56ad2e490d50e30628a70cbc81bfd13
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-08-02 09:55:37 +0100

    Made the grid adjustable

--- etchasketch.js
+++ etchasketch.js
@@ -6,11 +6,12 @@ const gridSizeButton = document.querySelector("#gridSizeButton");
 makeGrid(30);
 
 function makeGrid(x) {
+  container.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${x}, 1fr)`;
   for (let columns = 0; columns < x; columns++) {
     for (let rows = 0; rows < x; rows++) {
       const box = document.createElement("div");
       box.classList.add("sketch-divs");
-      box.setAttribute('style', 'aspect-ratio: 1/1; width: auto; height auto;');
       container.appendChild(box);
       box.addEventListener("mouseenter", function (e) {
         e.target.style.background = "pink";
